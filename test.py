321a23bd77b576a3237537b3a69173271b2c97ad
ikm216 <ikmclassof22@gmail.com>
2024-11-24 14:35:12 -0500

    feat: added convertTocds function to convert to comma delimited string

--- test.py
+++ test.py
@@ -10,7 +10,8 @@ def next_10(num):
     """
     return ",".join(str(num + i) for i in range(1, 11))
 
-
+def convertTocds(list):
+    return ",".join(list)
 
 def test_should_return_next_ten_numbers():
     assert next_10(2) == "3,4,5,6,7,8,9,10,11,12"
