15a08803f090d4581df64d5847bc35cb84d83182
ikm216 <ikmclassof22@gmail.com>
2024-11-24 14:44:52 -0500

    doc: added documentation to the feature convertTocds function

--- test.py
+++ test.py
@@ -11,7 +11,16 @@ def next_10(num):
     return ",".join(str(num + i) for i in range(1, 11))
 
 def convertTocds(list):
-    return ",".join(list)
+     """
+    Given a list, converts list as a comma-delimited string.
+
+    Args:
+        list: The list getting converted to a string.
+
+    Returns:
+        Comma-delimited string.
+    """
+     return ",".join(list)
 
 def test_should_return_next_ten_numbers():
     assert next_10(2) == "3,4,5,6,7,8,9,10,11,12"
