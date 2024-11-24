1f580015fbb58384f1a0509af3a4d258e4772481
ikm216 <ikmclassof22@gmail.com>
2024-11-24 14:17:30 -0500

    test: added test for coverting the list to a comma delimited string

--- test.py
+++ test.py
@@ -10,5 +10,10 @@ def next_10(num):
     """
     return ",".join(str(num + i) for i in range(1, 11))
 
+
+
 def test_should_return_next_ten_numbers():
     assert next_10(2) == "3,4,5,6,7,8,9,10,11,12"
+
+def test_should_return_comma_delimited_string():
+    assert convertTocds(["apple,bannana,orange"]) == "apple,bannana,orange"
